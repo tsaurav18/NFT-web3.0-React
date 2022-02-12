3bea685ddca49533dd16d05cfbda73798e286ad8
tsaurav18 <tsaurav1711@gmail.com>
2022-02-13 02:35:05 +0900

    nft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Header from "./components/Header";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PunkList from "./components/PunkList";
+import Main from "./components/Main";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xBAF0B68C1FC27D6C46536A894525bf33aAd94644&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+    return () => {};
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      {punkListData.length > 0 && (
+        <>
+          <Main selectedPunk={selectedPunk} punkListData={punkListData} />
+          <PunkList
+            punkListData={punkListData}
+            setSelectedPunk={setSelectedPunk}
+          />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,59 @@
+import React,{useEffect, useState} from "react";
+import instagramLogo from "../assets/owner/instagram.png";
+import twitterLogo from "../assets/owner/twitter.png";
+import moreIcon from "../assets/owner/more.png";
+import "./Main.css";
+const Main = ({selectedPunk, punkListData}) => {
+    console.log(punkListData)
+    const [activePunk, setActivePunk] = useState(punkListData[0])
+    useEffect(() => {
+      setActivePunk(punkListData[selectedPunk])
+      return () => {
+        
+      }
+    }, [punkListData,selectedPunk])
+    
+  return (
+    <div className="main">
+      <div className="mainContent">
+        <div className="punkHighlight">
+          <div className="punkContainer">
+            <img
+              className="selectedPunk"
+              src={activePunk.image_preview_url}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="punkDetails" style={{ color: "#fff" }}>
+          <div className="title">{activePunk.name}</div>
+          <span className="itemNumber">.#{activePunk.token_id}</span>
+        </div>
+        <div className="owner">
+          <div className="ownerImageContainer">
+            <img src={activePunk.owner.profile_img_url} alt="" />
+          </div>
+          <div className="ownerDetails">
+            <div className="ownerNameAndHandle">
+              <div className="ownerAddress">
+                {activePunk.owner.address}
+              </div>
+              <div className="ownerHandle">programmer</div>
+              <div className="ownerLink">
+                <img src={instagramLogo} alt="" />
+              </div>
+              <div className="ownerLink">
+                <img src={twitterLogo} alt="" />
+              </div>
+              <div className="ownerLink">
+                <img src={moreIcon} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
